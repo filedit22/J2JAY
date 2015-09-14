dc6b288929357a6d8dcd942398982262e651de09
BigBrainAFK <tony.langhammer@yahoo.de>
2015-09-14 19:46:27 +0200

    Initial release and upload
    First upload and release of the files

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+	//You can enter it manually of course
+	//example: $absolute_cache_path = "/httpdocs/statuschecker/";
+	//pls include a / at the end of the path if you type it manually
+	$absolute_cache_path = $_SERVER['DOCUMENT_ROOT'];
+	
+	//just keep the arrays like that!
+	//do not include http:// and no / at the end!
+	$servers = array(
+		array("id" => 1, "serveradress" => "pokemon-revolution.net", "port" => 80, "timeout" => 30, "displayname" => "PRO Server"),
+		array("id" => 2, "serveradress" => "46.28.204.197", "port" => 80, "timeout" => 30, "displayname" => "PRO Website")
+	);
+?>
