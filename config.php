95c0dafdeaa658fa29ee8bee43c1f118a4129836
BigBrainAFK <tony.langhammer@yahoo.de>
2015-09-25 23:29:01 +0200

    Config error fixed

--- config.php
+++ config.php
@@ -1,25 +1,19 @@
 <?php
-	//You can enter it manually of course
-	//example: $absolute_cache_path = "/httpdocs/statuschecker/";
-	//pls include a / at the end of the path if you type it manually
-    //$absolute_cache_path = $_SERVER['DOCUMENT_ROOT']."/yoursubfolder/";
-	$absolute_cache_path = $_SERVER['DOCUMENT_ROOT'];
-	
-	//just keep the arrays like that!
-	//do not include http:// and no / at the end!
-	$servers = array(
-<<<<<<< HEAD
-		array("id" => 1, "serveraddress" => "46.28.204.197", "port" => 800, "timeout" => 30, "displayname" => "PRO Server", "codecheck" => false, "keyphrase" => "<html>", "keyphrase2" => "</html>"),
-		array("id" => 2, "serveraddress" => "pokemon-revolution.net", "port" => 80, "timeout" => 30, "displayname" => "PRO Website", "codecheck" => true, "keyphrase" => "2015 ", "keyphrase2" => "/Creatures")
-=======
-		array("id" => 1, "serveradress" => "46.28.204.197", "port" => 80, "timeout" => 30, "displayname" => "PRO Server"),
-		array("id" => 2, "serveradress" => "pokemon-revolution.net", "port" => 80, "timeout" => 30, "displayname" => "PRO Website")
->>>>>>> master
-	);
-	
-	//set a key so only your cronjob can run this file!
-	$key = "insertyourkeyhere";
-<<<<<<< HEAD
+    //You can enter it manually of course
+    //example: $absolute_cache_path = "/httpdocs/statuschecker/";
+    //pls include a / at the end of the path if you type it manually
+    //$absolute_cache_path = $_SERVER['DOCUMENT_ROOT']."/yoursubfolder";
+    $absolute_cache_path = $_SERVER['DOCUMENT_ROOT'];
+
+    //just keep the arrays like that!
+    //do not include http:// and no / at the end!
+    $servers = array(
+        array("id" => 1, "serveraddress" => "46.28.204.197", "port" => 800, "timeout" => 30, "displayname" => "PRO Server", "codecheck" => false, "keyphrase" => "<html>", "keyphrase2" => "</html>"),
+        array("id" => 2, "serveraddress" => "pokemon-revolution.net", "port" => 80, "timeout" => 30, "displayname" => "PRO Website", "codecheck" => true, "keyphrase" => "2015 ", "keyphrase2" => "/Creatures")
+    );
+
+    //set a key so only your cronjob can run this file!
+    $key = "insertyourkeyhere";
 
     //enable/disable the "User Online" counter
     $onlinecounter = false;
@@ -33,6 +27,3 @@
     //and the amoutn of the selected format until it is considered offline
     $timeout_amount = 30;
 ?>
-=======
-?>
->>>>>>> master
